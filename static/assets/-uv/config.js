e5d289835a631e67bc5991b02c1a7699bc7a66ea
Hshdvdud <joebiden4729172@outlook.com>
2024-07-22 13:56:04 -0500

    Update config.js

--- static/assets/-uv/config.js
+++ static/assets/-uv/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-  prefix: "/a/",
-  bare: "/ov/",
+  prefix: "/bare/",
+  bare: "https://newsoftoday.tivita-dyndns.de/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/assets/-uv/handler.js?v=6-17-2024",
